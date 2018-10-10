296daf4180a7965148222a0750969d4f5d181b05
mwilli04 <mwilli04@L18H1MMWILLIAM4.aa.ad.epa.gov>
2018-10-10 11:41:02 -0400

    robotdrive depreciated, moving to DifferentialDrive

--- testbot_2019/src/org/usfirst/frc/team6004/robot/subsystems/Drivetrain.java
+++ testbot_2019/src/org/usfirst/frc/team6004/robot/subsystems/Drivetrain.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team6004.robot.RobotMap;
 import org.usfirst.frc.team6004.robot.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -19,17 +21,22 @@ public class Drivetrain extends Subsystem {
 	Victor rightRearDrive = new Victor(3);
 	
 	// Robot Drive
-	RobotDrive robotDrive = null;
+	DifferentialDrive robotDrive = null;
+	//RobotDrive robotDrive = null;
 	
 	public Drivetrain() {
 		// Victors
 		leftFrontDrive = new Victor(RobotMap.DRIVETRAIN_LEFT_FRONT_VICTOR);
 		leftRearDrive = new Victor(RobotMap.DRIVETRAIN_LEFT_BACK_VICTOR);
+		SpeedControllerGroup m_left = new SpeedControllerGroup(leftFrontDrive, leftRearDrive);
+		
 		rightFrontDrive = new Victor(RobotMap.DRIVETRAIN_RIGHT_FRONT_VICTOR);
 		rightRearDrive = new Victor(RobotMap.DRIVETRAIN_RIGHT_BACK_VICTOR);
+		SpeedControllerGroup m_right = new SpeedControllerGroup(rightFrontDrive, rightRearDrive);
+
 		
 		// Robot Drive
-		robotDrive = new RobotDrive(leftFrontDrive, leftRearDrive, rightFrontDrive, rightRearDrive);
+		robotDrive = new DifferentialDrive(m_left, m_right);
 	}
 	
 	public void tankDrive(double leftSpeed, double rightSpeed, boolean twitch) {
